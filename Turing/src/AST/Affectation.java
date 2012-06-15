ddbf07fa00122e3be7ba8a133f016f971ffd490b
patrickhdd <patrickhdd@4966d71d-c640-40c2-8cf4-ce08f407aa7f>
2012-06-15 11:10:20 +0000

    Prends la main ;)
    git-svn-id: svn+ssh://svn.code.sf.net/p/antc/code/trunk/hub_version@31 4966d71d-c640-40c2-8cf4-ce08f407aa7f

--- Turing/src/AST/Affectation.java
+++ Turing/src/AST/Affectation.java
@@ -18,11 +18,15 @@ public class Affectation extends Instruction {
         this.var = var;
         this.e = e;
     }
-    
+
     public Affectation(String varName, Expression e) {
         this.var = Variable.get(varName);
-        if(!var.getType().equals(e.getType())){
-            throw new UnsupportedOperationException("Type de " + varName + " (" + var.getType() +") non compatible avec (" + e.getType() +")");
+        if (!var.getType().equals(e.getType())) {
+            throw new UnsupportedOperationException("Type de " + varName + " (" + var.getType() + ") non compatible avec (" + e.getType() + ")");
+        }
+        if (var.getType().equals(Type.get("string")) && e.getType().equals(Type.get("int"))) {
+        } else {
+            throw new UnsupportedOperationException("Le cast n'est pas possible avec les types saisis."+ var.getType() + e.getType());
         }
         this.e = e;
     }
